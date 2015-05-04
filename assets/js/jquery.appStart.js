d1ddd65171f6b38f75ce272701496eb031c5f532
Liam Holmes <liamholmes@MacBook-Pro.local>
2015-05-04 11:02:36 +0100

    added the chat php include to several pages

--- assets/js/jquery.appStart.js
+++ assets/js/jquery.appStart.js
@@ -14,7 +14,7 @@
             //be sure to be same as colors on main.css or custom-variables.less
             colors : {
                 white: '#fff',
-                dark: '#4b89e4',
+                dark: '#2C3E50',
                 red: '#EF4836',
                 blue: '#1E8BC3',
                 green: '#3FC380',
--- calendar.php
+++ calendar.php
@@ -23,58 +23,6 @@ define('PAGE_KEYWORDS', '');
                        <!-- Import Chat -->
                         <?php include "php/components/global/chat.php"; ?>
                         
-                        <div class="chat-box">
-                            <h5>
-                                <span class="device"><i class="im-screen4"></i></span>
-                                <span class="status online"><i class="im-circle-small"></i></span> Mat Koubardaris
-                            </h5>
-                            <a id="close-user-chat" class="close" aria-hidden="true">&times;</a>
-                            <ul class="chat-messages">
-                                <li>
-                                    <div class="avatar">
-                                        <img src="assets/img/avatars/3.jpg" alt="@chadengle">
-                                    </div>
-                                    <div class="message">
-                                        <p class="chat-name">Mat Koubardaris <span class="chat-time">15 sec ago</span>
-                                        </p>
-                                        <p class="chat-txt">Hi Jack, are you still looking for a club in the UK?</p>
-                                    </div>
-                                </li>
-                                <li class="chat-me">
-                                    <div class="avatar">
-                                        <img src="assets/img/avatars/1.jpg" alt="@jonhdoe">
-                                    </div>
-                                    <div class="message">
-                                        <p class="chat-name">Jack Reid <span class="chat-time">10 sec ago</span>
-                                        </p>
-                                        <p class="chat-txt">Yeah mate, still looiking for the right club</p>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="avatar">
-                                        <img src="assets/img/avatars/3.jpg" alt="@chadengle">
-                                    </div>
-                                    <div class="message">
-                                        <p class="chat-name">Chad Engle <span class="chat-time">now</span>
-                                        </p>
-                                        <p class="chat-txt">Great, let me give you a call later today.</p>
-                                    </div>
-                                </li>
-                            </ul>
-                            <div class="chat-write">
-                                <form action="#" class="form-horizontal" role="form">
-                                    <div class="form-group">
-                                        <textarea name="sendmsg" id="sendMsg" class="form-control elastic" rows="1"></textarea>
-                                        <a role="button" class="btn" id="attach_photo_btn">
-                                            <i class="im-image2 s20"></i> 
-                                        </a>
-                                        <input type="file" name="attach_photo" id="attach_photo" class="unstyled">
-                                    </div>
-                                    <!-- End .form-group  -->
-                                </form>
-                            </div>
-                        </div>
-                        <!-- End chat-box -->
                     </div>
                     <!-- End sidebar-panel-content -->
                 </div>
